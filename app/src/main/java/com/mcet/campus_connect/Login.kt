97bc90a51d1696ad1fc2ab6d4e06a1be0ecfdb99
shaikhaleem11 <haleemshaik190@gmail.com>
2023-11-15 22:42:22 +0530

    firebased created

--- app/src/main/java/com/mcet/campus_connect/Login.kt
+++ app/src/main/java/com/mcet/campus_connect/Login.kt
@@ -8,6 +8,8 @@ import android.provider.ContactsContract.CommonDataKinds.Email
 
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
     private lateinit var edtEmail: EditText
@@ -15,9 +17,13 @@ class Login : AppCompatActivity() {
     private lateinit var btnLogin: Button
     private lateinit var btnSignUp: Button
 
+    private lateinit var mAuth:FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        mAuth =FirebaseAuth.getInstance()
 
         edtEmail = findViewById(R.id.edt_email)
         edtPassword = findViewById(R.id.edt_password)
@@ -29,6 +35,26 @@ class Login : AppCompatActivity() {
             val intent= Intent(this, SignUp::class.java)
             startActivity(intent)
         }
+        btnLogin.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val password =edtPassword.text.toString()
 
+            login(email,password);
+        }
+    }
+    private fun login(email:String,password:String){
+     //logic for logining user
+
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    //code for logging in user
+                    val intent = Intent(this@Login,MainActivity::class.java)
+                    startActivity(intent)
+
+                } else {
+                    Toast.makeText(this@Login,"suer does not exist", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
--- app/src/main/java/com/mcet/campus_connect/SignUp.kt
+++ app/src/main/java/com/mcet/campus_connect/SignUp.kt
@@ -1,11 +1,51 @@
 package com.mcet.campus_connect
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Telephony.Mms.Intents
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUp : AppCompatActivity() {
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var edtName: EditText
+    private lateinit var btnSignUp: Button
+    private lateinit var mAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        mAuth = FirebaseAuth.getInstance()
+        edtName = findViewById(R.id.edt_name)
+        edtEmail = findViewById(R.id.edt_email)
+        edtPassword = findViewById(R.id.edt_password)
+        btnSignUp = findViewById(R.id.btnSignup)
+
+        btnSignUp.setOnClickListener {
+            val email =edtEmail.text.toString()
+            val password =edtPassword.text.toString()
+
+            signup(email,password)
+        }
+    }
+
+    private fun signup(email:String,password:String){
+        //logic of creating user
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                   // code for jumping to home
+                    val intent = Intent(this@SignUp,MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@SignUp,"some error occured",Toast.LENGTH_SHORT).show()
+
+                }
+            }
     }
 }
