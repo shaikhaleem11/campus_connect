5412a656e899e449fefb085fffad1472333a7289
shaikhaleem11 <haleemshaik190@gmail.com>
2023-11-29 15:40:51 +0530

    on bind view ]done

--- app/src/main/java/com/mcet/campus_connect/MessageAdapter.kt
+++ app/src/main/java/com/mcet/campus_connect/MessageAdapter.kt
@@ -1,20 +1,47 @@
 package com.mcet.campus_connect
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
 
 class MessageAdapter(val context: Context, val messageList: ArrayList<Message> ) :
-    RecyclerView.Adapter<ViewHolder>{
+    RecyclerView.Adapter<ViewHolder>() {
+
+    val ITEM_RECEIVE =1
+    val ITEM_SENT = 2
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
+        if (viewType ==1){
+            //inflate receive
+            val view:View = LayoutInflater.from(context).inflate(R.layout.receive,parent, false)
+            return ReceiveViewHolder(view)
+
+        }else {
+            //inflate sent
+            val view:View =LayoutInflater.from(context).inflate(R.layout.sent,parent, false)
+            return SentViewHolder(view)
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        val currentMessage =messageList[position]
+
+        if(FirebaseAuth.getInstance().currentUser?.uid.equals(currentMessage.senderId)){
+            return ITEM_SENT
+        }else{
+            return ITEM_RECEIVE
+        }
     }
 
     override fun getItemCount(): Int {
+        return messageList.size
 
     }
 
